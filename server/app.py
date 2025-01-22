6630531d71c2604b9064b160cedc09c9657967de
vantiger69 <vantiger817@gmail.com>
2025-01-22 17:17:02 +0300

    complete_lab

--- server/app.py
+++ server/app.py
@@ -7,9 +7,32 @@ app = Flask(__name__)
 def index():
     return '<h1>Welcome to my page!</h1>'
 
-@app.route('/<string:username>')
-def user(username):
-    return f'<h1>Profile for {username}</h1>'
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(f"parameter received:{parameter}")
+    return f'<h1>print: {parameter}</h1>'
 
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    numbers  = [str(i) for i in range(parameter)]
+
+    output = '<br>'.join(numbers)
+    return f'<h1>count:{parameter}</h1><br>{output}'
+
+
+@app.route('/math/int:num1/int:num2/<string:operation>')
+def math(num1, num2, operation):
+    if operation == 'add':
+        result = num1 + num2
+    elif operation == 'subtract':
+        result = num1 - num2
+    elif operation == 'multiply':
+        result = num1 * num2
+    elif operation == 'divide':
+        result = num1 / num2
+    else:
+        return f'<h1> Invalid operation: {operation}</h1>'
+    
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
